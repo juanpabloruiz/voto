9520df09c90fbf562ec36959b93d8f9df2995878
juanpabloruiz <pabloruiz1980@gmail.com>
2025-08-10 00:29:52 -0300

    Agrego HTML al crudo PHP

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php
-include('conexion.php');
-?>
-<a href="votar.php">Votar</a>
+<?php include('conexion.php'); ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voto</title>
+</head>
+<body>
+    <a href="votar.php">Votar</a>
+</body>
+</html>
