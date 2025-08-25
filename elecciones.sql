125e6a93cada49bf0e2a452692aadfd238f5032a
juanpabloruiz <pabloruiz1980@gmail.com>
2025-08-25 10:26:44 -0300

    db updated

--- elecciones.sql
+++ elecciones.sql
@@ -1,102 +1,23 @@
--- phpMyAdmin SQL Dump
--- version 5.2.2deb1
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost:3306
--- Tiempo de generación: 12-08-2025 a las 22:46:50
--- Versión del servidor: 8.4.5-0ubuntu0.2
--- Versión de PHP: 8.4.5
+-- Adminer 5.3.0 MySQL 8.4.6-0ubuntu0.25.04.1 dump
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+SET NAMES utf8mb4;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `elecciones`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `token`
---
-
-CREATE TABLE `token` (
-  `id` int NOT NULL,
-  `codigo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `ip` varchar(45) COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `ubicacion` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `estado` int NOT NULL,
-  `creado` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
-
---
--- Volcado de datos para la tabla `token`
---
-
-INSERT INTO `token` (`id`, `codigo`, `ip`, `ubicacion`, `estado`, `creado`) VALUES
-(278, '38d390050a8b827f6eff5df5c4df27eb335e14c0bf76cd57fee260ccca34e1f4', '190.229.217.89', 'Argentina, Corrientes', 1, '2025-08-12 19:45:17');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `votos`
---
-
+DROP TABLE IF EXISTS `votos`;
 CREATE TABLE `votos` (
-  `id` int NOT NULL,
-  `candidato` varchar(50) COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `ip` varchar(45) COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `fecha` datetime NOT NULL
+  `id` int NOT NULL AUTO_INCREMENT,
+  `candidato` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `ip` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `ubicacion` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `estado` int NOT NULL,
+  `ingreso` datetime NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
---
--- Volcado de datos para la tabla `votos`
---
 
-INSERT INTO `votos` (`id`, `candidato`, `ip`, `fecha`) VALUES
-(94, 'massa', '190.229.217.89', '2025-08-12 19:45:19');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `token`
---
-ALTER TABLE `token`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `token` (`codigo`);
-
---
--- Indices de la tabla `votos`
---
-ALTER TABLE `votos`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `token`
---
-ALTER TABLE `token`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=279;
-
---
--- AUTO_INCREMENT de la tabla `votos`
---
-ALTER TABLE `votos`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=95;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- 2025-08-25 13:25:59 UTC
